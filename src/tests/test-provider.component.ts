63e579160ed37b39cb54c400b64a1dee7550e164
Levent Arman Özak <arman@armanozak.com>
2019-04-15 16:14:39 +0300

    Improve router outlet tests

--- src/tests/test-provider.component.ts
+++ src/tests/test-provider.component.ts
@@ -9,7 +9,7 @@ import { ContextMap } from '../lib/symbols';
   encapsulation: ViewEncapsulation.None,
 })
 export class TestProviderComponent {
-  private _title: string;
+  private _title: string = 'Testing';
 
   greeting$ = of('Hello');
   target = 'World';
